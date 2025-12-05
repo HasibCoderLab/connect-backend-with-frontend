8e82131a352d0900e9ef8a8d7c6359340c2ee004
Mohammad Hasib Hasan <coder.mohammadhasibhasan@gmail.com>
2025-12-05 21:31:53 +0600

    fetch data with axios

--- backend/index.js
+++ backend/index.js
@@ -3,12 +3,18 @@ import cors from "cors"
 
 
 const app  = express();
-app.use(cors())
+app.use(cors({
+    "origin": "http://localhost:5173"
+}))
 const port  = 8000;
 
 app.get("/",(req,res) =>{
     res.json({name:"Hasib" , age : 18});
-})
+});
+
+app.post("/",(req,res) =>{
+    res.send({success:true});
+});
 
 app.listen(port , () => {
     console.log(`Sarver is started at ${port}`);
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,46 +1,41 @@
-import React, { useState } from "react";
+import React from "react";
 
 const App = () => {
-  const [email, setEmail] = useState();
-  const [userName, setuserName] = useState();
-  const [password, setPassword] = useState();
-  //   const getRes = (e) =>{
-  //   e.preventDefault()
-  // }
-  async function getRes() {
-    let res = await fetch("http://localhost:8000/");
-    let data = await res.json();
-    console.log(data);
-    
-  }
-
   return (
     <div className="min-h-screen flex justify-center items-center bg-gray-700">
-  <form className="bg-white p-6 rounded-2xl shadow-md w-80 space-y-4">
-
+      <form className="bg-white p-6 rounded-2xl shadow-md w-80 space-y-4">
+        
         <h2 className="text-xl font-semibold text-gray-700 text-center">
-          Log in
+          Register
         </h2>
 
         <div className="flex flex-col space-y-3">
           <input
-            type="email" placeholder="Enter Email" className="border p-2 rounded-lg focus:outline-none focus:ring focus:ring-blue-300"
-            value={email} />
+            type="email"
+            placeholder="Enter Email"
+            className="border p-2 rounded-lg focus:outline-none focus:ring focus:ring-blue-300"
+          />
 
           <input
-            type="text" placeholder="Enter Username" className="border p-2 rounded-lg focus:outline-none focus:ring focus:ring-blue-300"
-            value={userName} />
+            type="text"
+            placeholder="Enter Username"
+            className="border p-2 rounded-lg focus:outline-none focus:ring focus:ring-blue-300"
+          />
 
           <input
-            type="password" placeholder="Enter Password" className="border p-2 rounded-lg focus:outline-none focus:ring focus:ring-blue-300"
-            value={password} />
+            type="password"
+            placeholder="Enter Password"
+            className="border p-2 rounded-lg focus:outline-none focus:ring focus:ring-blue-300"
+          />
         </div>
 
-        <button className="w-full bg-green-500 text-white py-2 rounded-lg cursor-pointer " onClick={() => getRes()}>
+        <button
+          type="submit"
+          className="w-full bg-red-400 text-white py-2 rounded-lg cursor-pointer"
+        >
           Submit
         </button>
-      </form> 
-      
+      </form>
     </div>
   );
 };
