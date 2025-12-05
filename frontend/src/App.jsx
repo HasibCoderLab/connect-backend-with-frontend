230fb1f5d7ecdb7b8f41580b80a65e8b2b0fbd59
Mohammad Hasib Hasan <coder.mohammadhasibhasan@gmail.com>
2025-12-05 20:35:59 +0600

    add useState

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,38 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 const App = () => {
+  const [email, setEmail] = useState();
+  const [userName, setuserName] = useState();
+  const [password, setPassword] = useState();
+
   return (
     <div className="min-h-screen flex justify-center items-center bg-gray-700">
       <form className="bg-white p-6 rounded-2xl shadow-md w-80 space-y-4">
-        
+
         <h2 className="text-xl font-semibold text-gray-700 text-center">
           Log in
         </h2>
 
         <div className="flex flex-col space-y-3">
           <input
-            type="email"
-            placeholder="Enter Email"
-            className="border p-2 rounded-lg focus:outline-none focus:ring focus:ring-blue-300"
-          />
+            type="email" placeholder="Enter Email" className="border p-2 rounded-lg focus:outline-none focus:ring focus:ring-blue-300"
+            value={email} />
 
           <input
-            type="text"
-            placeholder="Enter Username"
-            className="border p-2 rounded-lg focus:outline-none focus:ring focus:ring-blue-300"
-          />
+            type="text" placeholder="Enter Username" className="border p-2 rounded-lg focus:outline-none focus:ring focus:ring-blue-300"
+            value={userName} />
 
           <input
-            type="password"
-            placeholder="Enter Password"
-            className="border p-2 rounded-lg focus:outline-none focus:ring focus:ring-blue-300"
-          />
+            type="password" placeholder="Enter Password" className="border p-2 rounded-lg focus:outline-none focus:ring focus:ring-blue-300"
+            value={password} />
         </div>
 
-        <button
-          type="submit"
-          className="w-full bg-green-500 text-white py-2 rounded-lg "
-        >
+        <button className="w-full bg-green-500 text-white py-2 rounded-lg" >
           Submit
         </button>
       </form>
