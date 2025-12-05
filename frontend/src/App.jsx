321d297156f256f7b8e506c98f5102e35bd601f6
Mohammad Hasib Hasan <coder.mohammadhasibhasan@gmail.com>
2025-12-05 20:30:45 +0600

    style with tailwind

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div className="min-h-screen flex justify-center items-center bg-gray-700">
+      <form className="bg-white p-6 rounded-2xl shadow-md w-80 space-y-4">
+        
+        <h2 className="text-xl font-semibold text-gray-700 text-center">
+          Log in
+        </h2>
 
-export default App
+        <div className="flex flex-col space-y-3">
+          <input
+            type="email"
+            placeholder="Enter Email"
+            className="border p-2 rounded-lg focus:outline-none focus:ring focus:ring-blue-300"
+          />
+
+          <input
+            type="text"
+            placeholder="Enter Username"
+            className="border p-2 rounded-lg focus:outline-none focus:ring focus:ring-blue-300"
+          />
+
+          <input
+            type="password"
+            placeholder="Enter Password"
+            className="border p-2 rounded-lg focus:outline-none focus:ring focus:ring-blue-300"
+          />
+        </div>
+
+        <button
+          type="submit"
+          className="w-full bg-green-500 text-white py-2 rounded-lg "
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default App;
